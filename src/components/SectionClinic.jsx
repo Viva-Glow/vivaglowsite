04b055770f7630dbc1b31cf0c4cc387a0ed7a342
Laura Ortiz <bjtamyl@hotmail.com>
2023-10-13 18:59:47 +0200

    cards first structure

--- src/components/SectionClinic.jsx
+++ src/components/SectionClinic.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import "../css/cardsClinic.scss";
 
 const SectionClinic = () => {
   return (
-    <div>SectionClinic</div>
-  )
-}
+    <>
+      <section className="sectionClinic">
+        {/* 
+   
+    {/* card1------------------------------------- */}
+        <figure >
+          <h1>Tratamiento 1</h1>
+          <img
+            src="https://img.freepik.com/foto-gratis/persona-que-recibe-tratamiento-belleza-microagujas_23-2149334245.jpg?size=626&ext=jpg&ga=GA1.1.473773967.1686657261&semt=ais"
+            alt=""
+          />
+          <figcaption>
+            <h3>More Info</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur 
+            Descubre una clínica estética de vanguardia en Valencia. donde nuestros expertos profesionales te ofrecen tratamientos estéticos avanzados y también imparten cursos especializados en el ámbito de la bellezadddddddddddddddddd."
+            
+            </p>
+            <button>More Info</button>
+          </figcaption>
+        </figure>
+        {/* card2------------------------------------- */}
+        <figure class="image-block">
+          <h1>Tratamiento 2</h1>
+          <img
+            src="https://img.freepik.com/foto-gratis/mujer-relajante-salon-belleza_23-2148895559.jpg?size=626&ext=jpg&ga=GA1.1.473773967.1686657261&semt=ais"
+            alt=""
+          />
+          <figcaption>
+            <h3>More Info</h3>
+            <p>
+              Pellentesque habitant morbi tristique senectus et netus et
+              malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat
+              vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+              amet quam egestas semper. Aenean ultricies mi vitae est. Mauris
+              placerat eleifend leo.
+            </p>
+            <button>More Info</button>
+          </figcaption>
+        </figure>
+        {/* card3------------------------------------- */}
+        <figure class="image-block">
+          <h1>Tratamiento 3</h1>
+          <img
+            src="https://img.freepik.com/foto-gratis/retrato-mujer-cabello-morena-preparacion-rejuvenecimiento-operacion-cosmetologia-salon-belleza-manos-guantes-azules-dibujando-cara-botox-belleza_197531-2778.jpg?size=626&ext=jpg&ga=GA1.1.473773967.1686657261&semt=ais"
+            alt=""
+          />
+          <figcaption>
+            <h3>More Info</h3>
+            <p>
+              Pellentesque habitant morbi tristique senectus et netus et
+              malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat
+              vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+              amet quam egestas semper. Aenean ultricies mi vitae est. Mauris
+              placerat eleifend leo.
+            </p>
+            <button>More Info</button>
+          </figcaption>
+        </figure>
+      </section>
+    </>
+  );
+};
 
-export default SectionClinic
+export default SectionClinic;
