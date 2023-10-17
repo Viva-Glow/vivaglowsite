9fa11d4392a4bf0936005b67fec2e1202041d87a
DamiAbi <damithapam@gmail.com>
2023-10-17 13:25:17 -0300

    nav responsive

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,19 +1,30 @@
 import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+import '../css/slider.css'
+
 
 
 function Slider () {
   return (
     <Carousel fade className='ConteinerC' >
       <Carousel.Item >
-      <img className='Carousel' src="https://images.pexels.com/photos/7755654/pexels-photo-7755654.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-        <Carousel.Caption>
-          
+      <img className='Carousel' src="https://images.pexels.com/photos/6663456/pexels-photo-6663456.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <Carousel.Caption className=' BtnText'>
+          <h3 className='text'>NUEVOS TRATAMIENTOS</h3>
+        
+          <Button variant="secondary"  className='buttom'>Quiero saber más</Button>{' '}
         </Carousel.Caption>
       </Carousel.Item >
       <Carousel.Item >
-      <img className='Carousel' src="https://images.pexels.com/photos/9335961/pexels-photo-9335961.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-        <Carousel.Caption>
-       
+      <img className='Carousel' src="https://images.pexels.com/photos/5042628/pexels-photo-5042628.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        
+        
+        <Carousel.Caption className=' BtnText'>
+         
+        <h3 className='text'>CLASES DISPONIBLES</h3>
+         
+          
+          <Button variant="secondary" className='buttom'>Quiero saber más</Button>{' '}
         </Carousel.Caption>
       </Carousel.Item>
     
