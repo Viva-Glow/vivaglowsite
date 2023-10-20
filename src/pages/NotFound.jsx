39139b2c3a48b94ac95fbe2ca22f30c8857766b9
Laura Ortiz <bjtamyl@hotmail.com>
2023-10-20 15:32:39 +0200

    404 ready

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import notFound from "../assets/notFound.jpg";
+import "../css/404.css"
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="notF fixed-top">
+      <img src={notFound} className="card-img " alt="..." />
+      <div className="card-img-overlay ">
+        <div className="container text-white btn1  ">
+          <button
+            type="button "
+            className="btn  btn404 "
+          >
+            <a className=" text-center btnNot" href="/">
+             <b>Volver al inicio</b> 
+            </a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
--- src/routes/RoutesDos.jsx
+++ src/routes/RoutesDos.jsx
@@ -2,25 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
-// import About from "../components/About";
-// import Contact from "../components/Contact";
-// import Footer from "../components/Footer";
-// import SectionCourses from "../components/SectionCourses";
-// import SectionClinic from "../components/SectionClinic";
-// import Slider from "../components/Slider";
-
 
 const RoutesDos = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="*" element={<NotFound />} />
-      {/* <Route path="/slider" element={<Slider />} />
-        <Route path="/sectionClinic" element={<SectionClinic />} />
-        <Route path="/sectionCourses" element={<SectionCourses />} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/footer" element={<Footer />} /> */}
     </Routes>
   );
 };
